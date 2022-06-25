3da13feea42958167c53dce1c72e79553824a230
Krish18Joshi <krishjoshi791@gmail.com>
2022-06-25 12:48:44 +0530

    up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Krish joshi
+     <div className = "Container">
+     <div className = "Generator">
+      <h2 className='h2'>Password Generator</h2>
+      <div className = "generator_password">
+<h3>Password</h3>
+<button className='b1'></button>
+      </div>
+
+     </div>
+</div>
+
     </div>
   );
 }
