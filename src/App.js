73421490e8ff17fe4c8379279ff71bcbd9d2a443
Krish18Joshi <krishjoshi791@gmail.com>
2022-06-25 14:36:50 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/form.js';
 import ReactDOM from 'react-dom/client';
 function App() {
+
   return (
     <div className="App">
      <div className = "Container">
      <div className = "Generator">
       <h2 className='h2'>Password Generator</h2>
-      <div className = "generator_password">
-<h3>Password</h3>
-<button className='b1'><i className='far fa-clipboard'></i></button>
-      </div>
+      
       <div className='form'><Form /></div>
      </div>
 </div>
--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './form.css';
+
+
 
 function myFunction() {
     /* Get the text field */
@@ -14,37 +16,73 @@ function myFunction() {
   }
 
 function Form(){
+    const [password , setPassword ] =useState('');
+  const [passwordLength , setPasswordLength ] =useState(20);
+  const [uppercase , setUppercase ] =useState(false);
+  const [lowercase , setLowercase] =useState(false);
+  const [numbers , setNumbers] =useState(false);
+  const [symbols , setSymbols] =useState(false);
+
     return(
     <div className='for'>
+    <div className = "generator_password">
+<h3>{password}</h3>
+<button className='b1'><i className='far fa-clipboard'></i></button>
+      </div>
     <div className='form-group'>
     <label htmlFor='password-strength'>
 Password Strength
     </label>
-    <input type="number" id='password-sterngth' name='password-sterngth' max={20} min={5} ></input>
+    <input
+    defaultValue={passwordLength} 
+    onChange = {(e)=>{
+        setPasswordLength(e.target.value)
+    }}
+    type="number" id='password-sterngth' name='password-sterngth' max={20} min={5} ></input>
     </div>
     <div className='form-group'>
     <label htmlFor='uppercase-letters'>
 Include Uppercase Letters
     </label>
-    <input type="checkbox" id='uppercase-letters' name='uppercase-letters'  ></input>
+    <input 
+     checked={uppercase}
+     onChange = {(e)=>{
+        setUppercase(e.target.checked)
+    }}
+    type="checkbox" id='uppercase-letters' name='uppercase-letters'  ></input>
      </div>
      <div className='form-group'>
     <label htmlFor='lowercase-letters'>
 Include Lowercase Letters
     </label>
-    <input type="checkbox" id='lowercase-letters' name='lowercase-letters'  ></input>
+    <input 
+     checked={lowercase}
+     onChange = {(e)=>{
+        setLowercase(e.target.checked)
+    }}
+    type="checkbox" id='lowercase-letters' name='lowercase-letters'  ></input>
      </div>
      <div className='form-group'>
     <label htmlFor='numbers'>
 Include Numbers
     </label>
-    <input type="checkbox" id='numbers' name='numbers'  ></input>
+    <input
+     checked={numbers}
+     onChange = {(e)=>{
+        setNumbers(e.target.checked)
+    }}
+     type="checkbox" id='numbers' name='numbers'  ></input>
      </div>
      <div className='form-group'>
     <label htmlFor='symbols'>
 Include Symbols
     </label>
-    <input type="checkbox" id='symbols' name='symbols'  ></input>
+    <input
+     checked={symbols}
+     onChange = {(e)=>{
+        setSymbols(e.target.checked)
+    }}
+     type="checkbox" id='symbols' name='symbols'  ></input>
      </div>
      <button className='generator-b'> Generate - Password</button>
      </div>);
